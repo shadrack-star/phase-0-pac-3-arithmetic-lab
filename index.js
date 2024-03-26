92e65ad430bf6b352c6af2b9a01811898113b126
shadrack-star <shaddkiplangat98@gmail.com>
2024-03-26 14:15:42 +0300

    performing arithmethic operations

---
+++ index.js
@@ -0,0 +1,53 @@
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2 
+
+function add (a,b){
+    return a+b;
+
+}
+function subtract (a,b){
+    return a-b;
+
+}
+function multiply(a,b){
+    return a*b;
+
+}
+function divide(a,b){
+    return a/b;
+
+}
+let number= 5;
+
+function increment(n) {
+    return n + 1;
+  }
+  function decrement(n) {
+    return n - 1;
+  }
+ 
+ 
+   // parseInt("2"); //=> 2
+   // parseFloat("80.123999"); // 80.123999
+    
+  
+ 
+function makeInt(string) {
+    return parseInt(string, 10)
+  }
+  
+  function preserveDecimal(string) {
+    return parseFloat(string)
+  }
+
+/*
+
+function makeInt(string){
+    parseFloat(string); // 80.123999
+    
+}
+function preserveDecimal(string){
+    parseFloat(string); // 80.123999
+}*/
